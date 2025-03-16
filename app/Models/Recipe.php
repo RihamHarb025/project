561cf19c91a75bf4b165f9cf487c6a43841cf481
RihamHarb025 <rihamharbhk2005@gmail.com>
2025-03-16 22:45:29 +0200

    Initial commit

---
+++ app/Models/Recipe.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    //
+    protected $fillable = ['title','description','recipe-image','categories'];
+}
